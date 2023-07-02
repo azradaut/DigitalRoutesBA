3936b83c2b2d69c6f3b0a6b59d6f45def07a77d4
azra <azra.dautt@gmail.com>
2023-07-02 17:29:26 +0200

    Commit

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link} from 'react-router-dom';
 
 const Menu = ({cat}) => {
   const [posts, setPosts] = useState([]);
@@ -23,7 +24,9 @@ const Menu = ({cat}) => {
         <div className="post" key={post.id}>
             <img src={`../upload/${post?.img}`}  alt="" />
             <h2>{post.title}</h2>
-            <button>Read More</button>
+            <Link className='link' to={`/post/${post.id}`}>
+                <button>Read More</button>
+              </Link>
         </div>
        ))}
     </div>
