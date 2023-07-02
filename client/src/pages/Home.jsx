86fef1509aba2c41db8576d98665f147387a7f11
azra <azra.dautt@gmail.com>
2023-07-02 18:24:28 +0200

    Project skeleton

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useState } from "react";
+import React from "react";
 import { useEffect } from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
-import html from 'html';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -22,33 +21,33 @@ const Home = () => {
     fetchData();
   }, [cat]);
 
-  const getText = (html) => {
+
+  const getText = (html) =>{
     const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent;
+    return doc.body.textContent
   }
 
+
   return (
-    <div className='home'>
+    <div className="home">
       <div className="posts">
-        {posts.map(post => (
-          <div className='post' key={post.id}>
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
             <div className="img">
               <img src={`../upload/${post.img}`} alt="" />
             </div>
             <div className="content">
-              <Link className='link' to={`/post/${post.id}`}>
-              <h1>{post.title}</h1>
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
               </Link>
               <p>{getText(post.desc)}</p>
-              <Link className='link' to={`/post/${post.id}`}>
-                <button>Read More</button>
-              </Link>
+              <button>Read More</button>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
